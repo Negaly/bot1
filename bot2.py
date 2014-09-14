4706eddbd5789ba341d8d3eeae0be9fb2ea7bb3e
Negaly <iif_13m@hotmail.com>
2014-09-14 20:49:38 +0200

    v0.02

--- bot2.py
+++ bot2.py
@@ -1,34 +1,37 @@
 """"
-Question Discover Program
+bot podemos
 
 Tutorial program for PRAW:
 See https://github.com/praw-dev/praw/wiki/Writing-A-Bot/
+
+Autor: Negaly
 """
-
+#Importamos funciones basicas de bot y de temporizacion
 import time
-
 import praw
-
+#No se para que se hace esto, pero supongo que es para decirle a reddit que bot somos (no lo he cambiado pero lo hare en un futuro)
 r = praw.Reddit('PRAW related-question monitor by u/_Daimon_ v 1.0.'
                 'Url: https://praw.readthedocs.org/en/latest/'
                 'pages/writing_a_bot.html')
+
 r.login()
 already_done = []
 
 prawWords = ['praw', 'reddit_api', 'mellort']
-#while True:
-subreddit = r.get_subreddit('learnpython')
-
-
-for submission in subreddit.get_hot(limit=1):
-#if True:
-    print "vuelta"
-    op_text = submission.selftext.lower()
-    has_praw = any(string in op_text for string in prawWords)
-    r.send_message('Negaly', 'PRAW Thread', "msg")
-    # Test if it contains a PRAW-related question
-    #if submission.id not in already_done and has_praw:
-    #    msg = '[PRAW related thread](%s)' % submission.short_link
-    #    r.send_message('Negaly', 'PRAW Thread', msg)
-    #    already_done.append(submission.id)
-time.sleep(10)
+msg=""
+while True:
+    subreddit = r.get_subreddit('podemos')
+    for submission in subreddit.get_new(limit=10):
+        print "vuelta"
+        op_text = submission.selftext.lower()
+        #r.send_message('Negaly', 'PRAW Thread', "msg") 
+        #Manda mensajes bien
+        # Test if it contains a PRAW-related question
+        if submission.id not in already_done:
+            msg += submission.title 
+            #% submission.short_link
+          
+            already_done.append(submission.id)
+    r.send_message('Negaly', 'Titles', msg)
+    break    
+#time.sleep(1800)
