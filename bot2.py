23704d9eccc5b2802c8daac4515ac513e5ca0695
Negaly <iif_13m@hotmail.com>
2014-09-15 01:54:28 +0200

    v0.03

--- bot2.py
+++ bot2.py
@@ -9,6 +9,7 @@ Autor: Negaly
 #Importamos funciones basicas de bot y de temporizacion
 import time
 import praw
+
 #No se para que se hace esto, pero supongo que es para decirle a reddit que bot somos (no lo he cambiado pero lo hare en un futuro)
 r = praw.Reddit('PRAW related-question monitor by u/_Daimon_ v 1.0.'
                 'Url: https://praw.readthedocs.org/en/latest/'
@@ -18,20 +19,25 @@ r.login()
 already_done = []
 
 prawWords = ['praw', 'reddit_api', 'mellort']
-msg=""
+msg= ""
 while True:
     subreddit = r.get_subreddit('podemos')
     for submission in subreddit.get_new(limit=10):
         print "vuelta"
-        op_text = submission.selftext.lower()
-        #r.send_message('Negaly', 'PRAW Thread', "msg") 
-        #Manda mensajes bien
-        # Test if it contains a PRAW-related question
+        # Test if it contains a new post
         if submission.id not in already_done:
-            msg += submission.title 
-            #% submission.short_link
-          
+          #HIPERVINCULOS msg += '['+submission.title+'](%s)' % submission.short_link
+            msg += '%s \n' % submission.title
+
             already_done.append(submission.id)
-    r.send_message('Negaly', 'Titles', msg)
+    outfile = open('texto.txt', 'a') # Indicamos el valor 'w'.
+     
+    print (msg)
+
+    outfile.write(msg.encode("UTF-8"))
+    outfile.close()        
+
+
+    #r.send_message('Negaly', 'Titles', msg)
     break    
 #time.sleep(1800)
