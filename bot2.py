5f911c27f39ecb5ddc23d1bed14417fe2a3c608b
Negaly <iif_13m@hotmail.com>
2014-09-15 13:37:05 +0200

    0.11

--- bot2.py
+++ bot2.py
@@ -1,5 +1,5 @@
 """"
-bot podemos
+bot
 
 Tutorial program for PRAW:
 See https://github.com/praw-dev/praw/wiki/Writing-A-Bot/
@@ -10,21 +10,48 @@ Autor: Negaly
 import time
 import praw
 import pprint
+user = " "
+psswrd = " "
+subreddit = " "
+'''
+Iniciacion de variables
+'''
+def init():
+    infile = open('config.txt', 'r') # Indicamos el valor 'w'.
+    arraylineas = infile.readlines()    
+    user = str(arraylineas[0].split(" ")[1])
+    psswrd = str(arraylineas[1].split(" ")[1])
+    subreddit = str(arraylineas[2].split(" ")[1])
+    r.login(username = user.rstrip(),password = psswrd.rstrip())
+
+    infile.close()
+    return subreddit 
 '''
 def buscaPropuestas(limite):
     @limite permite seleccionar cuantos hilos busca en cada vuelta
-
+ unicode a UTF-8 -> .encode("UTF-8")
     buscaPropuestas recorre los ultimos hilos, buscando cuales pertenecen a la categoria propuestas.
 '''
 def buscaPropuestas(limite):
     msg= ""
-    for submission in subreddit.get_new(limit=limite):
+    infile = open('already_done.txt', 'r+') # Indicamos el valor 'w'.
+    already_done = infile.readlines()
+    print(already_done)
+    cont=1
+    numero= len(already_done)
+    for submission in subreddit.get_new(limit=limite):        
         # Test if it contains a new post
-        if submission.id not in already_done and (submission.link_flair_css_class=='propuesta'):
+        if submission.id+'\n' not in already_done and (submission.link_flair_css_class=='propuesta'):
           #HIPERVINCULOS msg += '['+submission.title+'](%s)' % submission.short_link
-            msg += '%s \n' % submission.title
-            already_done.append(submission.id)
-    outfile = open('texto.txt', 'a') # Indicamos el valor 'w'.
+            msg += str(len(already_done)+cont) 
+            msg += '  '
+            msg += submission.short_link
+            msg += ' %s ' % submission.title
+            msg += '\n'
+            cont += 1
+            infile.write(submission.id+'\n')
+    infile.close()
+    outfile = open('listadelinks.txt', 'a') # Indicamos el valor 'w'.
     outfile.write(msg.encode("UTF-8"))
     outfile.close()        
     #submission = r.get_submission(submission_id = "2ge35o")
@@ -32,17 +59,16 @@ def buscaPropuestas(limite):
     #print(submission.link_flair_text)
     #r.send_message('Negaly', 'Titles', msg)
     return msg
-
 '''Main'''
 #No se para que se hace esto, pero supongo que es para decirle a reddit que bot somos (no lo he cambiado pero lo hare en un futuro)
 r = praw.Reddit('PRAW related-question monitor by u/_Daimon_ v 1.0.'
                 'Url: https://praw.readthedocs.org/en/latest/'
                 'pages/writing_a_bot.html')
-r.login()
+subreddit = init()
 print "Inicio"
 while True:
-    already_done = []
     print ('vuelta')
-    subreddit = r.get_subreddit('podemos')
+    subreddit = r.get_subreddit(subreddit)
     buscaPropuestas(10)
+    break #Para ir haciendo pruebas
     time.sleep(1800)  
